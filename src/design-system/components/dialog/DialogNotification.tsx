596951b6d2817433509012e4f1487f17ea9bc4aa
Alberto Arias <alberto@grid.ai>
2022-05-09 15:32:49 -0400

    Feature(ui) Lai2-8884: Basic notification dialogs (#76)
    * Add dialog notification icons
    
    * Add DialogNotification component and documentation

---
+++ src/design-system/components/dialog/DialogNotification.tsx
@@ -0,0 +1,52 @@
+import React, { ReactNode } from "react";
+import { Dialog, DialogTitle, DialogContent, DialogActions } from ".";
+import { Stack, Box, Typography } from "..";
+import { DialogProps } from "./Dialog";
+import successIcon from "./success.svg";
+import warningIcon from "./warning.svg";
+import errorIcon from "./error.svg";
+
+const getText: Record<DialogNotificationVariant, string> = {
+  success: "Success",
+  warning: "Are you sure?",
+  error: "Error",
+};
+
+const getSrcIcon: Record<DialogNotificationVariant, string> = {
+  success: successIcon,
+  warning: warningIcon,
+  error: errorIcon,
+};
+
+export type DialogNotificationVariant = "success" | "warning" | "error";
+
+export type DialogNotificationProps = {
+  message: string;
+  description: string;
+  variant: DialogNotificationVariant;
+  actions: ReactNode;
+} & Pick<DialogProps, "open" | "onClose">;
+
+export default function DialogNotification({ ...props }: DialogNotificationProps) {
+  const text = getText[props.variant];
+  const src = getSrcIcon[props.variant];
+  return (
+    <Dialog open={props.open} onClose={props.onClose}>
+      <DialogTitle text={text}></DialogTitle>
+      <DialogContent>
+        <Stack width={"400px"} justifyContent={"center"} alignItems={"center"} spacing={2.5}>
+          <Box component={"img"} width={"96px"} height={"96px"} src={src}></Box>
+          <Stack spacing={0.5} alignItems={"center"} textAlign={"center"}>
+            <Typography fontFamily={"UCity"} fontWeight={600} fontSize={"16px"} lineHeight={"20px"}>
+              {props.message}
+            </Typography>
+            <Typography fontFamily={"Roboto"} fontWeight={400} fontSize={"14px"} lineHeight={"20px"}>
+              {props.description}
+            </Typography>
+          </Stack>
+        </Stack>
+      </DialogContent>
+      <DialogActions>{props.actions}</DialogActions>
+    </Dialog>
+  );
+}
--- src/design-system/components/dialog/dialog.stories.tsx
+++ src/design-system/components/dialog/dialog.stories.tsx
@@ -2,7 +2,8 @@ import { ComponentMeta, ComponentStory } from "@storybook/react";
 import Dialog, { DialogProps } from "design-system/components/dialog/Dialog";
 import { DialogTitle, DialogContent, DialogActions } from "design-system/components/dialog";
 import Button from "design-system/components/button";
-import { Box } from "..";
+import { Box, Stack } from "..";
+import DialogNotification, { DialogNotificationProps } from "./DialogNotification";
 
 export default {
   title: "Components/Dialog",
@@ -23,6 +24,24 @@ export default {
       control: "boolean",
     },
     children: {
+      table: {
+        disable: true,
+      },
+    },
+    variant: {
+      defaultValue: "success",
+      options: ["success", "error", "warning"],
+      control: "select",
+    },
+    message: {
+      defaultValue: "Successfully uploaded",
+      control: "text",
+    },
+    description: {
+      defaultValue: "Your image has been uploaded to the tool",
+      control: "text",
+    },
+    actions: {
       table: {
         disable: true,
       },
@@ -52,3 +71,38 @@ const Template: ComponentStory<any> = (args: DialogProps) => {
 };
 
 export const Playground = Template.bind({});
+Playground.parameters = { controls: { include: ["open", "fullWidth"] } };
+
+const buttonOnClickHandler = (event: any) => event.stopPropagation();
+
+const getActions = {
+  success: (
+    <>
+      <Button text="Done" onClick={buttonOnClickHandler} fullWidth />
+    </>
+  ),
+  error: (
+    <>
+      <Button text="Report" color={"grey"} onClick={buttonOnClickHandler} fullWidth />
+      <Button text="Retry" onClick={buttonOnClickHandler} fullWidth />
+    </>
+  ),
+  warning: (
+    <>
+      <Button text="Back" color={"grey"} onClick={buttonOnClickHandler} fullWidth />
+      <Button text="Discard" onClick={buttonOnClickHandler} fullWidth />
+    </>
+  ),
+};
+
+const NotificationTemplate: ComponentStory<any> = (args: DialogNotificationProps) => {
+  const actions = (
+    <Stack width={"100%"} direction={"row"} spacing={1}>
+      {getActions[args.variant]}
+    </Stack>
+  );
+  return <DialogNotification {...args} actions={actions}></DialogNotification>;
+};
+
+export const Notifications = NotificationTemplate.bind({});
+Notifications.parameters = { controls: { include: ["open", "variant", "message", "description"] } };
