9216f83b00059f4de82f1076cba27d9ca6772740
Alec Merdler <alecmerdler@gmail.com>
2022-05-12 13:16:09 -0700

    Fix(ui) LAI2-8818: loading spinner for unavailable <iframe> (#84)
    Implements a simple check which calls 'fetch()' on the iframe's
    target URL and renders a loading spinnner if it does not return
    HTTP 200.
    
    NOTE: Because of browser security measures, we can only check
    URLs with the same hostname (CORS). Therefore, this will only
    work for the Streamlit/StaticWebFrontend components, and only
    in the cloud (where they are served using the same Ingress URL).
    Locally, we run these servers with different localhost ports,
    which the browser blocks.

--- src/components/IFrameRoute.spec.tsx
+++ src/components/IFrameRoute.spec.tsx
@@ -5,9 +5,45 @@ import IFrameRoute from "./IFrameRoute";
 
 describe("IFrameRoute", () => {
   it("renders an <iframe> element for the given route", () => {
-    mount(<IFrameRoute name="test" iframeTargetUrl="http://example.com" />);
+    mount(<IFrameRoute name="test" iframeTargetUrl="http://lightning.ai" />);
 
     cy.get("iframe").should("have.attr", "name", "test");
-    cy.get("iframe").should("have.attr", "src", "http://example.com");
+    cy.get("iframe").should("have.attr", "src", "http://lightning.ai");
+  });
+
+  it("renders loading spinner if <iframe> is not available", () => {
+    const targetUrl = `http://${window.location.host}/frontend`;
+
+    cy.intercept(targetUrl, { statusCode: 503 }).as("fetch");
+
+    mount(<IFrameRoute name="test" iframeTargetUrl={targetUrl} />);
+
+    cy.wait("@fetch");
+
+    cy.get("iframe").should("not.exist");
+    cy.get("[data-testid=loading-spinner]").should("be.visible");
+  });
+
+  it("does not render a loading spinner if the target URL is not fetchable", () => {
+    mount(<IFrameRoute name="test" iframeTargetUrl="http://lightning.ai" />);
+
+    cy.get("[data-testid=loading-spinner]").should("not.exist");
+    cy.get("iframe").should("be.visible");
+  });
+
+  it("refetches the target URL on an interval to check if it is still available", () => {
+    const targetUrl = `http://${window.location.host}/frontend`;
+
+    cy.clock();
+
+    cy.intercept(targetUrl, { statusCode: 503 }).as("fetch");
+
+    mount(<IFrameRoute name="test" iframeTargetUrl={targetUrl} />);
+
+    new Array(5).fill(0).forEach(() => {
+      cy.tick(1000);
+
+      cy.wait("@fetch");
+    });
   });
 });
--- src/components/IFrameRoute.tsx
+++ src/components/IFrameRoute.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
-import { Alert, Box } from "design-system/components";
+import { Stack, Box } from "@mui/material";
+import { Alert, CircularProgress } from "design-system/components";
 
 import useLightningState from "hooks/useLightningState";
 import useUpdateLightningState from "hooks/useUpdateLightningState";
@@ -15,9 +16,18 @@ const isEstablishCommunicationMessage = (message: MessageEvent, iframeTargetUrl:
   );
 };
 
+/**
+ * Determines if we can execute a `fetch()` request to the given URL.
+ * Cross origin requests are blocked by the browser, so not all URLs can be fetched.
+ */
+const isFetchable = (url: string) => new URL(url).host === window.location.host;
+
+const refetchInterval = 1000;
+
 export default function IFrameRoute(props: Props) {
   const [iframePort, setIframePort] = useState<MessagePort | undefined>(undefined);
   const iframeRef = useRef<HTMLIFrameElement>(null);
+  const [isAvailable, setIsAvailable] = useState(false);
   const lightningState = useLightningState();
 
   const lightningStateMutation = useUpdateLightningState();
@@ -48,21 +58,45 @@ export default function IFrameRoute(props: Props) {
     }
   }, [iframePort, lightningState.data]);
 
+  useEffect(() => {
+    if (props.iframeTargetUrl && isFetchable(props.iframeTargetUrl)) {
+      const interval = setInterval(() => {
+        fetch(props.iframeTargetUrl).then(res => {
+          if (res.status === 200) {
+            setIsAvailable(true);
+          } else {
+            setIsAvailable(false);
+          }
+        });
+      }, refetchInterval);
+
+      return () => clearInterval(interval);
+    } else {
+      setIsAvailable(true);
+    }
+  }, [props.iframeTargetUrl]);
+
   return (
     <Box display={"flex"} flexDirection={"column"} height={"100%"}>
       <Alert title={"Warning"} severity="warning" show={lightningStateMutation.isError}>
         Failed to update, please try again.
       </Alert>
-      <Box
-        height={"100%"}
-        width={"100%"}
-        component={"iframe"}
-        sx={{ border: "none" }}
-        name={props.name}
-        src={props.iframeTargetUrl}
-        title={props.name}
-        ref={iframeRef}
-      />
+      {isAvailable ? (
+        <Box
+          height={"100%"}
+          width={"100%"}
+          component={"iframe"}
+          sx={{ border: "none" }}
+          name={props.name}
+          src={props.iframeTargetUrl}
+          title={props.name}
+          ref={iframeRef}
+        />
+      ) : (
+        <Stack alignItems={"center"} justifyContent={"center"} height={"100%"}>
+          <CircularProgress data-testid="loading-spinner" />
+        </Stack>
+      )}
     </Box>
   );
 }
