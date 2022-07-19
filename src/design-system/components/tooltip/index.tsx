bcc95be3db0fcd0d11fbaa9261e6835ab9ebcd00
Pritam Soni <23050213+pritamsoni-hsr@users.noreply.github.com>
2022-07-19 15:11:28 +0530

    add width prop to tooltip (#106)

--- src/design-system/components/tooltip/index.tsx
+++ src/design-system/components/tooltip/index.tsx
@@ -1,12 +1,15 @@
 import MuiTooltip, { TooltipProps as MuiTooltipProps } from "@mui/material/Tooltip";
 import { Box } from "..";
 
-export type TooltipProps = Partial<Pick<MuiTooltipProps, "title">> & Pick<MuiTooltipProps, "children" | "placement">;
+export type TooltipProps = Partial<Pick<MuiTooltipProps, "title">> &
+  Pick<MuiTooltipProps, "children" | "placement"> & {
+    width?: number | string;
+  };
 
-const Tooltip = ({ title = "", children, placement = "top" }: TooltipProps) => {
+const Tooltip = ({ title = "", children, placement = "top", width }: TooltipProps) => {
   return (
     <MuiTooltip title={title} placement={placement}>
-      <Box component={"span"} sx={{ cursor: title ? "pointer" : "inherit" }}>
+      <Box component={"span"} sx={{ cursor: title ? "pointer" : "inherit", width }}>
         {children}
       </Box>
     </MuiTooltip>
