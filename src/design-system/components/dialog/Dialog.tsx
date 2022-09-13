040f280131e13535eac946c65a7d0bf69f18c022
Pritam Soni <23050213+pritamsoni-hsr@users.noreply.github.com>
2022-09-13 17:41:39 +0530

    fix: LAI2-10519 add dialog radius (#122)

--- src/design-system/components/dialog/Dialog.tsx
+++ src/design-system/components/dialog/Dialog.tsx
@@ -2,6 +2,13 @@ import MuiDialog, { DialogProps as MuiDialogProps } from "@mui/material/Dialog";
 
 export type DialogProps = Pick<MuiDialogProps, "open" | "children" | "fullWidth" | "onClose">;
 
-const Dialog = (props: DialogProps) => <MuiDialog {...props} />;
+const Dialog = (props: DialogProps) => (
+  <MuiDialog
+    PaperProps={{
+      style: { borderRadius: "8px" },
+    }}
+    {...props}
+  />
+);
 
 export default Dialog;
