9d45f6b1f45b89c8eb1414cfaf1ea471b3c57bab
Pritam Soni <23050213+pritamsoni-hsr@users.noreply.github.com>
2022-09-29 15:45:49 +0530

    fix: make footer responsive, and update design/text (#129)

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,16 @@
 import React from "react";
-import { Stack, Box } from "design-system/components";
+import { Typography, Stack, Box, Paper } from "design-system/components";
 
 import LightningLogo from "resources/images/lightning-logo-with-text.svg";
 
 export default function Footer() {
   return (
-    <Stack
-      direction={"row"}
-      justifyContent={"space-between"}
-      alignItems={"center"}
-      left={20}
-      bottom={20}
-      position={"absolute"}>
-      <Box component={"img"} src={LightningLogo} alt="Lightning.ai" />
-    </Stack>
+    <Paper elevation={1} sx={{ position: "absolute", left: 0, right: 0, bottom: 0, paddingX: 2.5, paddingY: 1.5 }}>
+      <Stack direction={"row"} justifyContent={{ xs: "center", md: "flex-end" }} alignItems={"center"}>
+        <Typography>Running on</Typography>
+        &nbsp;
+        <Box component={"img"} src={LightningLogo} alt="Lightning.ai" />
+      </Stack>
+    </Paper>
   );
 }
