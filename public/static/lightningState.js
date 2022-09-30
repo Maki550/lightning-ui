bce771277579e9a3d8a5c1316ba4ec3e812ef020
Alberto Arias <alberto@grid.ai>
2022-09-30 14:56:54 +0200

    Allow communication within FF (#133)

--- public/static/lightningState.js
+++ public/static/lightningState.js
@@ -1,6 +1,7 @@
 (function () {
   const channel = new MessageChannel();
-  window.top.postMessage("Establish communication", window.location.ancestorOrigins[0], [channel.port2]);
+  // We use window.location.origin as we expect the apps belongs to the same origin that the Parent containing the iframe
+  window.top.postMessage("Establish communication", window.location.origin, [channel.port2]);
   class LightningState {
     static subscribe(componentHandler) {
       channel.port1.onmessage = message => {
