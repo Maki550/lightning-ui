7f862d67f3eb1e6974aa4b6892359df585674788
Alec Merdler <alecmerdler@gmail.com>
2022-10-05 14:19:58 +0100

    Fix Lightning State Script (#137)
    Removes the unnecessary 'postMessage' from the lightningState.js
    script we provide for app developers to hook their custom
    frontends into the Lightning App state (/api/v1/state). Sending
    'Subscribed' to the backend was causing it to overwrite the
    entire app state.
    
    We should start discouraging app developers from using the
    Lightning App state directly in their frontend, and instead
    use 'configure_api()' to easily create API endpoints for their
    apps, complete with OpenAPI definitions and validation.

--- public/static/lightningState.js
+++ public/static/lightningState.js
@@ -2,19 +2,22 @@
   const channel = new MessageChannel();
   // We use window.location.origin as we expect the apps belongs to the same origin that the Parent containing the iframe
   window.top.postMessage("Establish communication", window.location.origin, [channel.port2]);
+
   class LightningState {
     static subscribe(componentHandler) {
       channel.port1.onmessage = message => {
         componentHandler(message.data);
       };
-      channel.port1.postMessage("Subscribed");
+
       return () => {
         channel.port1.onmessage = null;
       };
     }
+
     static next(state) {
       channel.port1.postMessage(state);
     }
   }
+
   window.LightningState = LightningState;
 })();
