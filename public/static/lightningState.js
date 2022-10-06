017ec0db95f99ca1da875b23cbea1016cbf6f07b
Alec Merdler <alecmerdler@gmail.com>
2022-10-06 01:31:43 +0100

    Fix 'targetOrigin' for LightningState.js (#139)
    Fixes a regression introduced when adding support for Firefox.
    Adds conditional logic to detect Firefox using 'navigator.userAgent'.

--- public/static/lightningState.js
+++ public/static/lightningState.js
@@ -1,7 +1,13 @@
 (function () {
   const channel = new MessageChannel();
+
+  const targetOrigin = window.navigator.userAgent.includes("Firefox")
+    ? window.location.origin
+    : /* eslint-disable-next-line compat/compat */
+      window.location.ancestorOrigins[0];
+
   // We use window.location.origin as we expect the apps belongs to the same origin that the Parent containing the iframe
-  window.top.postMessage("Establish communication", window.location.origin, [channel.port2]);
+  window.top.postMessage("Establish communication", targetOrigin, [channel.port2]);
 
   class LightningState {
     static subscribe(componentHandler) {
